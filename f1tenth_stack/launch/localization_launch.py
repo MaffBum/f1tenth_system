1e9a597c5cf86321503549d95cf69e2e5b784158
Matt <mffr@student.unimelb.edu.au>
2025-04-02 16:14:07 +1100

    updated localisation_launch file

--- f1tenth_stack/launch/localization_launch.py
+++ f1tenth_stack/launch/localization_launch.py
@@ -64,7 +64,7 @@ def generate_launch_description():
 
         DeclareLaunchArgument(
             'map',
-            default_value='/home/f1jetson/f1tenth_ws/src/f1tenth_system/f1tenth_stack/maps/31march.yaml',
+            default_value='/home/f1jetson/f1tenth_ws/src/f1tenth_system/f1tenth_stack/maps/2April.yaml',
             description='Full path to map yaml file to load'),
 
         DeclareLaunchArgument(
